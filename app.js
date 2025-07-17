1b92805e1e87644ac6f08eacfff3275f92beae48
Amches <dan8363@gmail.com>
2025-07-17 16:54:34 +0300

    initial submition

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const auth = require("./middlewares/auth");
 const { NOT_FOUND } = require("./utils/errors");
 
 const errorHandler = require("./middlewares/error-handler");
+const { requestLogger, errorLogger } = require("./middlewares/logger");
 
 const itemsRouter = require("./routes/clothingItems");
 const usersRouter = require("./routes/users");
@@ -20,9 +21,16 @@ mongoose
   })
   .catch(console.error);
 
+app.get("/crash-test", () => {
+  setTimeout(() => {
+    throw new Error("Server will crash now");
+  }, 0);
+});
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(requestLogger);
 app.post("/signin", login);
 app.post("/signup", createUser);
 
@@ -37,6 +45,8 @@ app.use(
   },
   itemsRouter
 );
+
+app.use(errorLogger);
 
 app.use((req, res) =>
   res.status(NOT_FOUND).send({ message: "Item Id not Found " })
